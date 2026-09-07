5a0747a6887024c8e531ef565ff8a862b8bdee56
Yakushenko Anton Aleksandrovich <75640559+YakushenkoAnton773@users.noreply.github.com>
2026-09-08 02:04:51 +0400

    Пишем первые gRPC API-тесты. Практическое задание: Получаем данные текущего пользователя через gRPC

--- tests/grpc_users_test.go
+++ tests/grpc_users_test.go
@@ -141,3 +141,73 @@ func TestGRPCUpdateUser(t *testing.T) {
 
 	t.Logf("updated user with ID %s", response.GetUser().GetId())
 }
+
+func TestGRPCGetUserMe(t *testing.T) {
+	userRequest := &v1.CreateUserRequest{
+		Email:      gofakeit.Email(),
+		Password:   gofakeit.Password(true, true, true, true, false, 12),
+		LastName:   gofakeit.LastName(),
+		FirstName:  gofakeit.FirstName(),
+		MiddleName: gofakeit.FirstName(),
+	}
+
+	connection, err := grpc.NewClient(
+		"localhost:9000",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	require.NoError(t, err)
+	defer connection.Close()
+
+	usersClient := v1.NewUsersServiceClient(connection)
+	authenticationClient := v1.NewAuthenticationServiceClient(connection)
+
+	createdUser, err := usersClient.CreateUser(
+		context.Background(),
+		userRequest,
+	)
+
+	require.NoError(t, err)
+	require.NotNil(t, createdUser)
+	require.NotNil(t, createdUser.GetUser())
+	require.NotEmpty(t, createdUser.GetUser().GetId())
+
+	authRequest := &v1.LoginRequest{
+		Email:    userRequest.GetEmail(),
+		Password: userRequest.GetPassword(),
+	}
+
+	loginResponse, err := authenticationClient.Login(
+		context.Background(),
+		authRequest,
+	)
+
+	require.NoError(t, err)
+	require.NotNil(t, loginResponse)
+	require.NotNil(t, loginResponse.GetToken())
+	require.NotEmpty(t, loginResponse.GetToken().GetAccessToken())
+
+	authorizationMetadata := metadata.Pairs(
+		"authorization",
+		"Bearer "+loginResponse.GetToken().GetAccessToken(),
+	)
+
+	contextWithToken := metadata.NewOutgoingContext(
+		context.Background(),
+		authorizationMetadata,
+	)
+
+	response, err := usersClient.GetMe(
+		contextWithToken,
+		&v1.Empty{},
+	)
+
+	require.NoError(t, err)
+	require.NotNil(t, response)
+	require.NotNil(t, response.GetUser())
+
+	assert.Equal(t, createdUser.GetUser().GetId(), response.GetUser().GetId())
+	assert.Equal(t, createdUser.GetUser().GetEmail(), response.GetUser().GetEmail())
+	assert.Equal(t, createdUser.GetUser().GetLastName(), response.GetUser().GetLastName())
+	assert.Equal(t, createdUser.GetUser().GetFirstName(), response.GetUser().GetFirstName())
+	assert.Equal(t, createdUser.GetUser().GetMiddleName(), response.GetUser().GetMiddleName())
+}
